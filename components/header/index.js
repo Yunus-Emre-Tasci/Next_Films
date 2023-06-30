5b5a6357ba6f09e622aa25165105f85d804b8432
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-06-30 14:48:37 +0300

    header

--- components/header/index.js
+++ components/header/index.js
@@ -1,8 +1,31 @@
+import Link from 'next/link'
 import React from 'react'
+import {FaPlayCircle} from "react-icons/fa"
+import styles from "./styles.module.css"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header  className = {
+        `${styles.header} container fluid`
+    } >
+        <div className = {
+            styles.heaederWrapper
+        } >
+            <Link Link href = "/"
+            className = {
+                styles.logo
+            } >
+                < FaPlayCircle/> NEXTFILMS
+            </Link>
+            <nav className = {
+                styles.navigationMenu
+            } >
+                <Link href="/">Movies</Link>
+                <Link href="/">Series</Link>
+                <Link href="/">Kids</Link>
+            </nav>
+        </div>
+    </header>
   )
 }
 
