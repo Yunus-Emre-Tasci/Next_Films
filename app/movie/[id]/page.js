d3733fb5c2d41f614c1ec415b3d6a72c179de275
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-06-30 19:14:44 +0300

    not-found, error

---
+++ app/movie/[id]/page.js
@@ -0,0 +1,36 @@
+import {
+    MovieContainer
+} from "@/containers/movie"
+import Movies from "@/mocks/movies.json"
+import {
+    notFound
+} from "next/navigation"
+
+async function Delay(ms){
+    return new Promise(resolve=>setTimeout(resolve,ms))
+}
+
+async function MoviePage({
+    params,searchParams
+}) {
+    await Delay(4000)
+    console.log(params);
+
+    const movieDetail = Movies.results.find(movie => movie.id.toString() === params.id)
+
+    if (!movieDetail) {
+        notFound()
+    }
+    if (searchParams.error==="true"){
+        throw new Error("Error happened")
+    }
+
+    return ( <
+        MovieContainer movie = {
+            movieDetail
+        }
+        />
+    )
+}
+
+export default MoviePage
