9c6f0e91eb13721e2dc00a357c5cc5e36eb06e04
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-06-30 14:34:21 +0300

    footer-header

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,10 @@
 import { Inter } from 'next/font/google'
+
 import "@/styles/reset.css"
 import "@/styles/global.css"
+
+import Header from "@/components/header"
+import Footer from "@/components/footer"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,8 +15,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="tr">
-      <body className={inter.className}>{children}</body>
+    <html lang="tr" className={inter.className}>
+      <body className="container">
+        <Header/>
+        <main>{children}</main>
+        <Footer/>
+        </body>
     </html>
   )
 }
